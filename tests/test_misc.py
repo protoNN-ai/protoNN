067a2bdd0164a00d693d552ede49d1ba29929300
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-12-27 16:51:33 +0900

    unittests

---
+++ tests/test_misc.py
@@ -0,0 +1,12 @@
+"""Tests for misc"""
+import unittest
+import logging
+import protonn
+logger = logging.getLogger(__name__)
+
+
+class Tests(unittest.TestCase):
+
+    def test_main(self):
+        logger.info("test dummy")
+

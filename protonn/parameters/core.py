f209f4cc4a2af771579e85175137eec35c18214a
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-01-16 15:48:20 +0900

    observe as a call

--- protonn/parameters/core.py
+++ protonn/parameters/core.py
@@ -1,4 +1,36 @@
+import inspect
+import ast
+
+#  some singleton class here to keep track of all parameter
+
+
 def view(scope):
     """this function shold be used as a decorato"""
+
     print("test")
     return scope
+
+
+def observe(x, stack_level=1):
+    """register single variable for tracking"""
+
+    # print('value', x)
+    frame_info = inspect.getouterframes(inspect.currentframe())[stack_level]
+    #  caller_path = frame_info[1]  # frame_info.filename
+    caller_line = frame_info[2]  # lineno
+    caller = frame_info[0]  # frame
+    code = inspect.getsource(caller)
+    print(code, caller_line)
+    call_line = code.splitlines()[caller_line - 1].strip()
+    syntax = ast.parse(call_line)
+    var_name = None
+    for node in ast.walk(syntax):
+        if isinstance(node, ast.Call) and node.func.id == 'observe':
+            var_name = node.args[0].id
+    if var_name is None:
+        raise ValueError(ast.dump(syntax))
+    return var_name
+
+
+def dump(path):
+    print("dump not implemented yet")
--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -1,7 +1,7 @@
 """Tests for parameters"""
 import unittest
 import logging
-from protonn.parameters.core import view
+from protonn.parameters.core import view, observe
 
 logger = logging.getLogger(__name__)
 
@@ -9,10 +9,15 @@ logger = logging.getLogger(__name__)
 class Tests(unittest.TestCase):
 
     def test_observe(self):
-        logger.info("test_view")
+        logger.info("test view")
 
         @view
         def my_scope():
             x = 5
             return x
         my_scope()
+
+        logger.info("test observe")
+        y = 23
+        #name = observe(y)
+        #assert name == "y"

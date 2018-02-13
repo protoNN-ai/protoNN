07bc026ef9f4bcaf6569469c560d18f8f65e426e
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-02-13 18:34:45 +0900

    image set size stub

--- protonn/data/imaging/misc.py
+++ protonn/data/imaging/misc.py
@@ -11,3 +11,13 @@ def make_channels_last(img):
     axes = img.shape
     img = np.rollaxis(img, 0, len(axes))
     return img
+
+
+def set_size(img, shape):
+    res = img
+    # pad1 = dim_max - img.shape[0]
+    # pad1 = max(0, pad1)
+    # pad2 = dim_max - img.shape[1]
+    # pad2 = max(0, pad2)
+    # res = np.lib.pad(img, ((0, pad1), (0, pad2), (0, 0)), 'constant', constant_values=value)
+    return res

9847ece23a42742e81841b3d7fa909bcd5f2fb8c
Aleksandr Drozd <alexander.drozd@gmail.com>
2019-08-05 15:28:16 +0900

    update original parameters

--- protonn/optim/__main__.py
+++ protonn/optim/__main__.py
@@ -9,14 +9,20 @@ def product_dict(**kwargs):
 
 
 def main():
-    print("hi")
+    print("protonn optimizer")
+    # load yml with all parameters
+    params_original = {"a": 1, "padding": 7}
     # TODO: define syntax for specifying ranges
-    params = {}
-    params["aligner_left_eye"] = [[x, x] for x in [0.30, 0.31]]
-    params["padding"] = [0, 0.5, 1.0, 1.5]
-    for param_instance in product_dict(**params):
-        print(param_instance)
-
+    params_ranges = {}
+    params_ranges["aligner_left_eye"] = [[x, x] for x in [0.30, 0.31]]
+    params_ranges["padding"] = [0, 0.5, 1.0, 1.5]
+    for param_instance in product_dict(**params_ranges):
+        params_result = params_original.copy()
+        params_result.update(param_instance)
+        print(params_result)
+        # inject modified parameters
+        # save unique file name
+        # run/schedule experiment
 
 
 if __name__ == "__main__":

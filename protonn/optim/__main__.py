6e724646db3e5bb203e04ea87028a5cb14656e5a
Aleksandr Drozd <alexander.drozd@gmail.com>
2019-08-05 15:11:12 +0900

    expand parameter ranges into product

--- protonn/optim/__main__.py
+++ protonn/optim/__main__.py
@@ -1,5 +1,22 @@
+import itertools
+
+
+def product_dict(**kwargs):
+    keys = kwargs.keys()
+    vals = kwargs.values()
+    for instance in itertools.product(*vals):
+        yield dict(zip(keys, instance))
+
+
 def main():
     print("hi")
+    # TODO: define syntax for specifying ranges
+    params = {}
+    params["aligner_left_eye"] = [[x, x] for x in [0.30, 0.31]]
+    params["padding"] = [0, 0.5, 1.0, 1.5]
+    for param_instance in product_dict(**params):
+        print(param_instance)
+
 
 
 if __name__ == "__main__":

434abada62000c1dea9a810ab6cb096d1f740077
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-01-08 15:24:34 +0900

    load dataframes from dir

--- protonn/vis.py
+++ protonn/vis.py
@@ -1,27 +1,44 @@
-import logging
-import os
 import pandas
+import json
+from pathlib import Path
 from pandas.io.json import json_normalize
-from vecto.utils.data import load_json
+
+
+def load_json(path):
+    f = open(path)
+    s_data = f.read()
+    data = json.loads(s_data)
+    f.close()
+    return data
 
 
 def df_from_file(path):
-    data = load_json(path)
-    # meta = [["experiment_setup", "task"],
-    #         ["experiment_setup", "subcategory"],
-    #         ["experiment_setup", "method"],
-    #         ["experiment_setup", "embeddings"]]
+    try:
+        data = load_json(path)
+    except Exception as e:
+        print(f"can't load {str(path)}")
+        raise e
     dframe = json_normalize(data)
-    #if "details" in dframe:
-    #    dframe.drop("details", axis="columns", inplace=True)
-    #default_measurement = "accuracy"
-    #try:
-    #    default_measurement = dframe["experiment_setup.default_measurement"].unique()[0]
-    #except KeyError:
-    #    logger.warning(f"default_measurement not specified in {path}")
-    #dframe["result"] = dframe["result." + default_measurement]
-    # df["reciprocal_rank"] = 1 / (df["rank"] + 1)
+
+    # TODO: drop heavy columns before or after normalization
     return dframe
 
-df = df_from_file("/home/blackbird/Projects/Performance/log_debug/1.json")
+
+def df_from_dir(path):
+    dfs = []
+    dfs = [df_from_file(f) for f in path.glob("**/*.json")]
+    if len(dfs) == 0:
+        raise RuntimeError("no data to load")
+    dframe = pandas.concat(dfs, sort=True)
+    return dframe
+
+
+def filter_by(df, filters):
+    df_plot = df
+    for key in filters:
+        df_plot = df_plot[df_plot[key] == filters[key]]
+    return df_plot
+
+
+df = df_from_dir(Path("/home/blackbird/Projects/Performance/log_debug"))
 print(df)

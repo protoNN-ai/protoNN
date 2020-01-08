075b43543101a34fd55e35640bff08a5ef248316
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-01-08 16:59:48 +0900

    wip

--- protonn/vis.py
+++ protonn/vis.py
@@ -1,6 +1,5 @@
 import pandas
 import json
-from pathlib import Path
 from pandas.io.json import json_normalize
 
 
@@ -33,15 +32,56 @@ def df_from_dir(path):
     return dframe
 
 
-def pivot_dataframe(df, key_target, key_primary, key_secondary):
-    groupby_items = [key_secondary, key_primary]
-    group = df.groupby(groupby_items)
-    means = group.mean()
-    means.reset_index(inplace=True)
-    means = means.loc[:, groupby_items + [key_target]]
-    # TODO: aggregate according to strategy for each column individually 
-    unstacked = means.groupby(groupby_items)[key_target].aggregate('first').unstack()
-    return unstacked
+class PivotTable():
+    def __init__(self,
+                 key_target,
+                 key_primary,
+                 key_secondary,
+                 keys_maximize=[],
+                 keys_average=[],
+                 keys_drop=[]):
+        self.key_target = key_target
+        self.key_primary = key_primary
+        self.key_secondary = key_secondary
+        self.keys_maximize = keys_maximize
+        self.keys_average = keys_average
+        self.keys_drop = keys_drop
+
+    def get_maxed(self, df):
+        return df
+
+    def get_averaged(self, df):
+        print("averaging")
+        print(df)
+        print("becomes")
+        g = df.groupby(self.groupby_items + self.keys_average)
+        r = g.mean()
+        #r = df.mean()
+        print(r)
+        print()
+        return r
+
+    def pivot_dataframe(self, df):
+        # TODO: warn about unknown keys
+        self.groupby_items = [self.key_secondary, self.key_primary]
+        group = df.groupby(self.groupby_items)
+        r = group.apply(self.get_averaged)
+        return r
+        #group = df.groupby("device")
+        #means = group.agg({framew})
+        #means = group.agg({"device": "first"})
+        # for i in group:
+        #     print("g")
+        #     ip[]
+        #     print(type(i[1]))
+        #     print(i)
+        # print(group.aggregate("first"))
+        exit(1)
+        means.reset_index(inplace=True)
+        means = means.loc[:, groupby_items + [key_target]]
+        # TODO: aggregate according to strategy for each column individually 
+        unstacked = means.groupby(groupby_items)[key_target].aggregate('first').unstack()
+        return unstacked
 
 
 def filter_by(df, filters):

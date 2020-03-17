c25649e590a487eb1ea2a0267bfa168696b14c93
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-03-17 15:32:18 +0900

    lines

--- protonn/vis/__init__.py
+++ protonn/vis/__init__.py
@@ -1,1 +1,2 @@
 from .vis import df_from_file, df_from_dir, filter_by, PivotTable
+from .lines import plot_lines
---
+++ protonn/vis/lines.py
@@ -0,0 +1,34 @@
+from protonn.vis import df_from_dir, PivotTable, filter_by
+from protonn.vis.styles import colors, line_styles
+from matplotlib import pyplot as plt
+
+
+def plot_lines(df, key_target, key_first, key_second, key_third):
+    # print(key_target, key_first, key_second, key_third)
+    vals_third = sorted(df[key_third].unique())
+    dic_style = {f: s for f, s in zip(vals_third, line_styles)}
+    # TODO: move this to extra args
+    x_ticks = None
+    vals_second = df[key_second].unique()
+    for color, value_second in zip(colors, vals_second):
+        # print("plotting ", device)
+        df_filtered = filter_by(df, {key_second: value_second})
+        pt = PivotTable(key_target=key_target,
+                        key_primary=key_third,
+                        key_secondary=key_first,
+                        keys_average=[])
+        df_mean, df_max, df_std = pt.pivot_dataframe(df_filtered)
+        # print(df_mean)
+        if x_ticks is None:
+            x_ticks = df_mean.index
+        for col in sorted(df_mean.columns):
+            plt.plot(df_mean.index, df_mean[col],
+                     linestyle=dic_style[col],
+                     linewidth=1.2,
+                     color=color,
+                     label=f"{value_second} / {col}")
+    plt.ylabel(key_target)
+    #plt.xticks(x_ticks[8:], rotation=90)
+    # TODO: make this customi
+    plt.xlabel(key_first)
+    plt.legend()

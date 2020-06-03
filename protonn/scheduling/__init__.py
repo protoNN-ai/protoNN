ae37ddb214de77525430f60961f04f0d4134a7e9
Sasha <alexander.drozd@gmail.com>
2020-06-03 13:21:09 +0900

    better job script

--- protonn/scheduling/__init__.py
+++ protonn/scheduling/__init__.py
@@ -1,12 +1,36 @@
+import os
+import subprocess
 import uuid
 
 
-def schedule_job(command):
-    path_scripts = "~/.local/protonn/scripts"
+def run_locally(command):
+    proc = subprocess.Popen(command, shell=False)
+    proc.communicate()
+
+
+def schedule_job(command, job_name="job_name"):
+    path_home = os.path.expanduser("~")
+    path_scripts = os.path.join(path_home, ".local/protonn/scripts")
     os.makedirs(path_scripts, exist_ok=True)
+    # TODO: use project name + timestamp + short random str
     unique_name = uuid.uuid4().hex + ".sh"
     path_script = os.path.join(path_scripts, unique_name)
-    # write launch params to job file
-    # run job scheduler
+    # get this params from some options
     with open(path_script, "w") as f:
+        f.write("#!/bin/bash\n")
+        f.write("#$ -cwd\n")
+        f.write("#$ -l rt_F=1\n")
+        f.write("#$ -l h_rt=4:00:00\n")
+        f.write(f"#$ -N {job_name}\n")
+        f.write("#$ -j y\n")
+        f.write("#$ -o $JOB_NAME.o$JOB_ID\n")
+        f.write("\nsource ~/.bashrc\n")
+        # TODO: do nice line breaks
         f.write(" ".join(map(str, command)))
+        f.write("\n")
+        f.write("path_scrypt=$(pwd)/$0\n")
+        f.write("\nrm $path_scrypt\n")
+        f.write("\n")
+
+    os.chmod(path_script, 0o766)
+    # run job scheduler

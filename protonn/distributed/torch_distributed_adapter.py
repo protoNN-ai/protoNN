f39c76568d6f6d75513de49cb4ebe6ee60b16f7a
Emil VATAI <emil.vatai@gmail.com>
2021-12-07 12:06:30 +0900

    Add `torch_distributed_adapter`

---
+++ protonn/distributed/torch_distributed_adapter.py
@@ -0,0 +1,42 @@
+from typing import Optional
+
+import torch
+import torch.distributed as dist
+
+ALLREDUCE_OPS = [dist.ReduceOp.SUM]
+
+
+def init() -> None:
+    if dist.is_mpi_available():
+        backend = "mpi"
+    elif (
+        torch.cuda.is_available()
+        and dist.is_nccl_available()
+        and torch.cuda.device_cound() > 0
+    ):
+        backend = "nccl"
+    elif dist.is_gloo_available():
+        backend = "gloo"
+    dist.init_process_group(backend)
+
+
+def rank() -> int:
+    return dist.get_rank()
+
+
+def world_size() -> int:
+    return dist.get_world_size()
+
+
+def allreduce(tensor: torch.Tensor, op: Optional[int]) -> torch.Tensor:
+    if op is None:
+        dist.all_reduce(tensor)
+        tensor /= dist.get_world_size()
+    else:
+        dist.all_reduce(tensor, op=ALLREDUCE_OPS[op])
+    return tensor
+
+
+def broadcast(tensor: torch.Tensor, src_rank: int) -> torch.Tensor:
+    dist.broadcast(tensor, src_rank)
+    return tensor

9e9361b12d8756cf2560846b4b100f73b34cbe57
Sasha <alexander.drozd@gmail.com>
2022-08-05 07:06:23 +0900

    remember name_task

--- protonn/experiment_config.py
+++ protonn/experiment_config.py
@@ -36,6 +36,7 @@ def load_yaml_config(path_config):
 
 class BaseConfig(dict):
     def __init__(self, name_task, cluster_env, param_path=None):
+        self["name_task"] = name_task
         if len(sys.argv) < 2 and param_path is None:
             print("run main.py config.yaml")
             print("or")

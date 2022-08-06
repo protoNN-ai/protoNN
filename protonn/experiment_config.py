301df67e71ddb391675633e09a7431623f808d83
Sasha <alexander.drozd@gmail.com>
2022-08-06 09:14:02 +0900

    set up accelerator

--- protonn/experiment_config.py
+++ protonn/experiment_config.py
@@ -37,7 +37,11 @@ def load_yaml_config(path_config):
 class BaseConfig(dict):
     def __init__(self, name_task, cluster_env, param_path=None):
         self["name_task"] = name_task
-        self["cnt_gpus_per_node"] = int(os.environ["NUM_GPUS_PER_NODE"])
+        if int(os.environ["NUM_GPUS_PER_NODE"]) > 0:
+            self["cnt_gpus_per_node"] = -1
+            self["accelerator"] = "gpu"
+        else:
+            self["cnt_gpus_per_node"] = 0
         if len(sys.argv) < 2 and param_path is None:
             print("run main.py config.yaml")
             print("or")
